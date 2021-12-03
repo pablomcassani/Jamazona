262b61e2271af44a84ad5e7eeaa8ff042af0f527
pablomcassani <pablomcassani@gmail.com>
2021-12-03 00:22:31 -0300

    first commit

---
+++ Readme.txt.txt
@@ -0,0 +1,5 @@
+El E-commerce usa node.js como servidor
+Abrir una consola en la carpeta jamazona y hacer un npm start para el back end.
+Después abrir una consola en la carpeta frontend y hacer npm start parar abrir el e-commerce.
+
+Gracias.
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,23 @@
-const express = require("express");
-const data =require("./data.js");
+import express from 'express'; //gives access to express package.
+import cors from 'cors';//bypass cors protocols
+import data from './data';//let you control products from backend.
+
 const app = express();
+
+app.use(cors());
 
 app.get("/api/products", (req, res) =>{
 	res.send(data.products);
-});
+}); //returns the adress in server.js.
+app.get('/api/products/:id', (req, res)=> {
+	const product = data.products.find((x) =>x._id === req.params.id);
+		if  (product){
+			res.send(product);
+	}	else {
+			res.status(404).send( {message: 'product Not Found'});
+		}
+	});
 
 app.listen(5000, () => {
 	console.log('serve at http://localhost:5000');
-});
+});//crea webserver
